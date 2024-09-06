940405852f5288f97a68467ea33e0785eb848aef
Marijana <marijanailiev@gmail.com>
2024-09-06 19:59:23 +0200

    i add a print on line 99

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -96,6 +96,7 @@ def change_player(current_player, board):
 
 def main():
     print("This is the best game that we ever made ")
+    print("This is a print just to check myself if im dumb as it seems")
     board = [["-", "-", "-", ],
              ["-", "-", "-", ],
              ["-", "-", "-"]]
