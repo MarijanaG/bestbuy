64839bcfbb724cf3d3e604b0aa4828b10130664d
Marijana <marijanailiev@gmail.com>
2024-09-30 22:08:00 +0200

    Application store done

--- BestBuy/main.py
+++ BestBuy/main.py
@@ -1,32 +1,64 @@
 import products
 from store import Store
 
-bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = products.Product("MacBook Air M2", price=1450, quantity=100)
-
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
-
-bose.show()
-mac.show()
-
-bose.set_quantity(1000)
-bose.show()
+def create_default_inventory():
+    # setup initial stock of inventory
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250)
+                    ]
+    return product_list
 
 
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
-               ]
+def start(store: Store):
+    while True:
+        print("Welcome to our store")
+        print("1.List all products in store")
+        print("2.Show total amount in store")
+        print("3.Make an order")
+        print("4.Quit")
 
-store = Store()
+        user_choice = input("Please enter your choice: ")
 
-for product in product_list:
-    store.add_product(product)
+        if user_choice == "1":
+            products = store.get_all_products()
+            if products:
+                print("Available products ")
+                for product in products:
+                    print(product)
+            else:
+                print("No active products available")
+        elif user_choice == "2":
+            total_amount = store.get_total_quantity()
+            print(f"Total amount of quantity is {total_amount}")
+        elif user_choice == "3":
+            shopping_list = []
+            while True:
+                product_name = input("Please enter the product name, or write 'done' to finish ")
+                if product_name.lower() == "done":
+                    break
+                product = next((p for p in store.products if p.name.lower() == product_name.lower()), None)
 
-active_products = store.get_all_products()
+                if product:
+                    quantity = int(input("Enter quantity: "))
+                    shopping_list.append((product, quantity))
+                else:
+                    print(f"Product not found in store ")
 
-products_store = store.get_all_products()
-print(store.get_total_quantity())
-print(store.order([(products_store[0], 1), (products_store[1], 2)]))
+        elif user_choice == "4":
+            print("Thanks for using application store ")
+            break
+        else:
+            print("invalid choice ")
+
+
+if __name__ == "__main__":
+    store = Store()
+    default_inventory = create_default_inventory()
+
+    # Add products to the store
+    for product in default_inventory:
+        store.add_product(product)
+
+    # Start the menu
+    start(store)
